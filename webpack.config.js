a0a84db348520e5d5253046c993bd30b016ca6fb
swlws <979623074@qq.com>
2025-06-04 11:12:17 +0800

    play

--- webpack.config.js
+++ webpack.config.js
@@ -9,91 +9,97 @@ import json5 from 'json5';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export default {
-  mode: 'development',
-  // entry: './src/index.js',
-  entry: {
-    index: './src/index.js',
-    // main: './src/main.js',
-  },
-  output: {
-    // filename: 'bundle.js',
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    publicPath: './',
-  },
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      // css
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      // 图片
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      // 字体
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      // 数据
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ['csv-loader'],
-      },
-      {
-        test: /\.xml$/i,
-        use: ['xml-loader'],
-      },
-      // 配置文件
-      {
-        test: /\.toml$/i,
-        type: 'json',
-        parser: {
-          parse: toml.parse,
+export default (env) => {
+  console.log(`webpack env:`);
+  console.log(JSON.stringify(env, null, 2));
+  console.log('================\n');
+
+  return {
+    mode: 'development',
+    // entry: './src/index.js',
+    entry: {
+      index: './src/index.js',
+      // main: './src/main.js',
+    },
+    output: {
+      // filename: 'bundle.js',
+      filename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+      publicPath: './',
+    },
+    devtool: 'inline-source-map',
+    module: {
+      rules: [
+        // css
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
         },
-      },
-      {
-        test: /\.yaml$/i,
-        type: 'json',
-        parser: {
-          parse: yaml.parse,
+        // 图片
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
         },
-      },
-      {
-        test: /\.json5$/i,
-        type: 'json',
-        parser: {
-          parse: json5.parse,
+        // 字体
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
         },
-      },
+        // 数据
+        {
+          test: /\.(csv|tsv)$/i,
+          use: ['csv-loader'],
+        },
+        {
+          test: /\.xml$/i,
+          use: ['xml-loader'],
+        },
+        // 配置文件
+        {
+          test: /\.toml$/i,
+          type: 'json',
+          parser: {
+            parse: toml.parse,
+          },
+        },
+        {
+          test: /\.yaml$/i,
+          type: 'json',
+          parser: {
+            parse: yaml.parse,
+          },
+        },
+        {
+          test: /\.json5$/i,
+          type: 'json',
+          parser: {
+            parse: json5.parse,
+          },
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'webpack playground',
+      }),
+      // new BundleAnalyzerPlugin(),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'webpack playground',
-    }),
-    // new BundleAnalyzerPlugin(),
-  ],
-  devServer: {
-    static: './dist',
-  },
-  optimization: {
-    runtimeChunk: 'single',
-    splitChunks: {
-      chunks: 'all',
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
+    devServer: {
+      static: './dist',
+    },
+    optimization: {
+      runtimeChunk: 'single',
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+          },
         },
       },
     },
-  },
+  };
 };
