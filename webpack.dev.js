91805b40d64df5c953a69b411d4013fdab2c5b27
swlws <979623074@qq.com>
2025-06-06 21:57:16 +0800

    ProvidePlugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -10,7 +10,11 @@ module.exports = (env) => {
     plugins: [
       // 全局模块，不再需要 import 引入
       new webpack.ProvidePlugin({
+        // 如果你遇到了至少一处用到 _ 变量的模块实例，那请你将 lodash package 引入进来，并将其提供给需要用到它的模块。
         _: 'lodash',
+        // 使用 ProvidePlugin 暴露出某个模块中单个导出，通过配置一个“数组路径”（例如 [module, child, ...children?]）实现此功能。
+        // 所以，我们假想如下，无论 join 方法在何处调用，我们都只会获取到 lodash 中提供的 join 方法。
+        join: ['lodash', 'join'],
       }),
     ],
     devServer: {
