c640e21bd772e53d2a506dcc78bb746bd5a359e4
swlws <979623074@qq.com>
2025-06-07 12:12:41 +0800

    config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,22 @@
+const fs = require('fs');
+const path = require('path');
 const baseConfig = require('./webpack.base');
 
 const { merge } = require('webpack-merge');
 
 module.exports = (env) => {
-  return merge(baseConfig(env), {
+  const config = merge(baseConfig(env), {
     mode: 'production',
     output: {
       // 生产模式时，这里必须为 ./  ，否则会报 Cannot GET /index.html
       publicPath: './',
     },
   });
+
+  fs.writeFileSync(
+    path.resolve(__dirname, '.prod.config.json'),
+    JSON.stringify(config, null, 2)
+  );
+
+  return config;
 };
